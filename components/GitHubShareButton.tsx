48bfcae35a6f16952d20920fe34eec70048c7f53
kivircikparantez <113123933+kivircikparantez@users.noreply.github.com>
2022-11-04 11:27:20 +0300

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/kivircikparantez'
+      href='https://github.com/kivircik-parantez'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
